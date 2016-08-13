0429d55fa84f794e6db8197ac9a7d9831606a060
Brett LaBombarda <bplabombarda@gmail.com>
2016-08-12 21:32:10 -0500

    removing dependency folders.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ require( './styles/main.scss' );
 
 // inject bundled Elm app into div#main
 var Elm = require( './Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+Elm.Main.embed( document.getElementById('main'));
