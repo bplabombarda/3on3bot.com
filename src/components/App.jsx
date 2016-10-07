e083efacea44a7b94a6fd46e6c27afe33238c0fb
Brett LaBombarda <bplabombarda@gmail.com>
2016-10-07 09:28:14 -0500

    Adjusting player component and oTGames state property.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export default class App extends Component {
 
         this.state = {
             date: null,
-            games: []
+            otGames: []
         };
     }
 
@@ -26,16 +26,43 @@ export default class App extends Component {
         }, () => {
             helpers.getGamesFromDate(this.state.date.format('YYYY-MM-DD'))
                 .then((response) => {
-                    let date = response.data.dates[0];
-                    if (date) {
-                        date.games.forEach((game) => {
+
+                    let gamesOnDate = response.data.dates[0];
+
+                    if (gamesOnDate) {
+
+                        let otGames = [];
+
+                        gamesOnDate.games.forEach((game) => {
+
                             if (game.linescore.currentPeriod === 4) {
-                                console.log(game);
-                                this.setState({
-                                    games: [game, ...this.state.games]
-                                });
+
+                                let goalHighlights = game.content.highlights.scoreboard.items;
+                                let srcHighlight = goalHighlights[goalHighlights.length - 1];
+
+                                let oTGameObj = {
+                                    away: {
+                                        teamName: game.teams.away.team.teamName,
+                                        locationName: game.teams.away.team.locationName,
+                                        abbr: game.teams.away.team.abbreviation
+                                    },
+                                    home: {
+                                        teamName: game.teams.home.team.teamName,
+                                        locationName: game.teams.home.team.locationName,
+                                        abbr: game.teams.home.team.abbreviation
+                                    },
+                                    title: goalHighlights[goalHighlights.length - 1].title,
+                                    source: srcHighlight.playbacks[srcHighlight.playbacks.length - 1].url,
+                                };
+
+                                otGames = [oTGameObj, ...otGames];
                             }
                         });
+
+                        this.setState({
+                            otGames: otGames
+                        });
+
                     } else {
                         console.log('No games today!');
                     }
@@ -51,11 +78,18 @@ export default class App extends Component {
     render() {
         return (
             <div className="container">
-                <DatePicker
-                    dateFormat="MM-DD-YY"
-                    selected={moment(this.state.date)}
-                    onChange={this.handleDateChange.bind(this)} />
+                <header>
+
+                    <DatePicker
+                        dateFormat="MM-DD-YY"
+                        selected={moment(this.state.date)}
+                        onChange={this.handleDateChange.bind(this)}
+                        />
+
+                </header>
+
                 <Player autostart={false} slug={'klingberg-picks-corner-in-ot'} topic={'277984386'} content={'41089003'}/>
+                
             </div>
         );
     }
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,10 +4,15 @@ require('./Player.scss');
 
 export default class Player extends Component {
     render() {
-        let url = `https://www.nhl.com/video/embed/${this.props.slug}/c-${this.props.content}?autostart=${this.props.autostart}`;
+        let url = `http://md-akc.med.nhl.com/mp4/nhl/2016/02/10/15dc395a-f8b1-401c-af0b-2113156a1090/1455076513168/asset_1200k.mp4`;
+        let title = 'Domingue robs Panarin twice'
+
         return (
-            <div className="iframeContainer">
-                <iframe src={url} width='540' height='304' frameBorder="0"></iframe>
+            <div className="videoContainer">
+                <video controls autoPlay name="media">
+                    <source src={ url } type="video/mp4"/>
+                </video>
+                <h3 className='videoTitle'>{ title }</h3>
             </div>
         );
     }
